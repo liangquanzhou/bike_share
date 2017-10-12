0db10a1c9e4c708600e4776fa3018c1151225176
Liangquan Zhou <liangquanzhou@gmail.com>
2017-10-11 23:19:15 -0400

    tweak and add data files

--- run.R
+++ run.R
@@ -1,3 +1,4 @@
 source("src/Regression.R")
 rmarkdown::render("report/report.Rmd")
-# xaringan::infinite_moon_reader("report/presentation/presentation.Rmd")
+xaringan::infinite_moon_reader("docs/index.Rmd")
+# to stop server use: ?servr::daemon_stop
